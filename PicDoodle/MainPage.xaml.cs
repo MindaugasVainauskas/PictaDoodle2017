c7edea51338cc57775fa087e1f6e1028e93cd467
M.Vainauskas <mindelis1@gmail.com>
2017-03-30 19:46:25 +0100

    Photo capture image merge now working.  Attempt to use gallery image end with unauthorised access exception.

--- PicDoodle/MainPage.xaml.cs
+++ PicDoodle/MainPage.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
 using Windows.Media.Capture;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,24 +33,41 @@ namespace PicDoodle
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //Bitmap image that will be used for pthoto/image pictures to be displayed under inkcanvas
+        BitmapImage cvBitmapImage;
+
+        StorageFile imageFile;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            this.InitializeComponent();         
+            this.Loaded += MainPage_Loaded;
+            
+            grdImageCanvas.SizeChanged += GrdImageCanvas_SizeChanged;
+           
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //here I'm simply settign prefered size of window that application should launch in if on pc.
+            ApplicationView.PreferredLaunchViewSize = new Size(400, 650);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+           
 
             //load the inktoolbar with all the tools
             itbToolBar.Loaded += itbToolBar_Loaded;
-
             //load the inkCanvas and set the allowed input devices
             icvCanvas.Loaded += IcvCanvas_Loaded;
-            grdImageCanvas.SizeChanged += GrdImageCanvas_SizeChanged;
-           
         }
 
         //this event will change sizes of ink canvas and image to fit current window size
         private void GrdImageCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             icvCanvas.Width = Window.Current.Bounds.Width;
+            icvCanvas.Height = Window.Current.Bounds.Width * 0.8; 
+            //not sure which width setting would be more optimal for image          
             imgPicture.Width = Window.Current.Bounds.Width;
+            imgPicture.Height = Window.Current.Bounds.Width * 0.8;           
         }
 
         //method to load inkcanvas supported input devices
@@ -72,40 +95,52 @@ namespace PicDoodle
 
         }
 
+        byte[] _buffer;
+
         //event handler to open camera and take a picture
         private async void Photo_Click(object sender, RoutedEventArgs e)
         {
             CameraCaptureUI photoCapture = new CameraCaptureUI();
-            photoCapture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            photoCapture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Png;           
             photoCapture.PhotoSettings.CroppedAspectRatio = new Size(5, 4);
-            photoCapture.PhotoSettings.MaxResolution = CameraCaptureUIMaxPhotoResolution.HighestAvailable;
+            photoCapture.PhotoSettings.MaxResolution = CameraCaptureUIMaxPhotoResolution.HighestAvailable;           
 
-            StorageFile photoStored = await photoCapture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            imageFile = await photoCapture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            
 
             //picture capture cancelled
-            if (photoStored == null)
+            if (imageFile == null)
             {
                 return;
             }
             else
             {
+               
                 //get the image stream from camera
                 IRandomAccessStream imageStream;
-                imageStream = await photoStored.OpenAsync(FileAccessMode.Read);
+                imageStream = await imageFile.OpenAsync(FileAccessMode.ReadWrite);
                 
                 //make new bitmap image
-                BitmapImage cvBitmapImage = new BitmapImage();
+                cvBitmapImage = new BitmapImage();
 
                 //set bitmapimage source to file stream
                 cvBitmapImage.SetSource(imageStream);
                 //set image source to bitmap image
                 imgPicture.Source = cvBitmapImage;
 
-               
+
+                RandomAccessStreamReference rasr = RandomAccessStreamReference.CreateFromStream(imageStream);//.CreateFromUri(cvBitmapImage.UriSource);
+                var streamWithContent = await rasr.OpenReadAsync();
+                _buffer = new byte[streamWithContent.Size];
+                await streamWithContent.ReadAsync(_buffer.AsBuffer(), (uint)streamWithContent.Size, InputStreamOptions.None);
+
+
             }
 
             
         }
+
+      
 
         //event handler to pick picture from gallery
         private async void Open_Click(object sender, RoutedEventArgs e)
@@ -122,25 +157,34 @@ namespace PicDoodle
             imagePicker.FileTypeFilter.Add(".png");
 
             //wait for an image to be selected
-            var image = await imagePicker.PickSingleFileAsync();
+            imageFile = await imagePicker.PickSingleFileAsync();
 
-            if (image != null)
-            {                              
+            if (imageFile != null)
+            {
+               
                 //add image at runtime
-                using (IRandomAccessStream imageStream = await image.OpenAsync(FileAccessMode.Read))
+                using (IRandomAccessStream imageStream = await imageFile.OpenAsync(FileAccessMode.ReadWrite))
                 {
+
                     //make new bitmap image
-                    BitmapImage cvBitmapImage = new BitmapImage();
-                   
+                    cvBitmapImage = new BitmapImage();
+ 
                     //set bitmapimage source to file stream
                     cvBitmapImage.SetSource(imageStream);
                     //set image source to bitmap image
                     imgPicture.Source = cvBitmapImage;
                     imgPicture.Stretch = Stretch.Fill;
-                   
+
+
+                    
+                    RandomAccessStreamReference rasr = RandomAccessStreamReference.CreateFromStream(imageStream);
+                    var streamWithContent = await rasr.OpenReadAsync();
+                    _buffer = new byte[streamWithContent.Size];
+                    await streamWithContent.ReadAsync(_buffer.AsBuffer(), (uint)streamWithContent.Size, InputStreamOptions.None);
+
                 }
-                
-               
+
+
             }
             else // user cancelled image selection
             {
@@ -149,15 +193,60 @@ namespace PicDoodle
         }
 
         //event handler to save image
-        private void Save_Click(object sender, RoutedEventArgs e)
+        private async void Save_Click(object sender, RoutedEventArgs e)
         {
+            //should merge inkcanvas strokes and underlying image to single image file
+            //after merging it will save the resulting combo file to picture gallery
 
-        }
+            //Get the picture gallery folder
+            StorageFolder imageStorage = KnownFolders.SavedPictures;
+            //create file for new image
+            var mergedImage = await imageStorage.CreateFileAsync("pictaDoodle_" + DateTime.Now.ToFileTime() + ".png", CreationCollisionOption.ReplaceExisting);
 
+            //create canvasDevice
+            CanvasDevice cvDevice = CanvasDevice.GetSharedDevice();
+            //Create canvas render target
+            CanvasRenderTarget cvrTarget = new CanvasRenderTarget(cvDevice, (int)icvCanvas.Width, (int)(icvCanvas.Width*0.8), 96);
+                       
+            //start the drawing session
+            using (var ds = cvrTarget.CreateDrawingSession())
+            {
+                //set destination rectangle that will determine the size of the editable image in application
+                Rect rectangle = new Rect(0, 0, icvCanvas.Width, (icvCanvas.Width*0.8));
+               
+                ds.Clear(Colors.White);
+
+                var editableImage = await CanvasBitmap.LoadAsync(cvDevice, imageFile.Path);
+                //var editableImage = CanvasBitmap.CreateFromBytes(cvDevice.Device, _buffer,
+                //                                                 (int)imgPicture.Width, (int)imgPicture.Height,
+                //                                                 Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized);
+
+                //var swBmp = new SoftwareBitmap(BitmapPixelFormat.Bgra8, (int)cvrTarget.SizeInPixels.Width, 
+                //                                (int)cvrTarget.SizeInPixels.Height);
+
+                //swBmp.CopyFromBuffer(_buffer.AsBuffer/*());*/
+              //  var editableImage = CanvasBitmap.CreateFromSoftwareBitmap(cvDevice.Device, swBmp);
+
+                ds.DrawImage(editableImage, rectangle);
+                ds.DrawInk(icvCanvas.InkPresenter.StrokeContainer.GetStrokes());
+                }
+
+                using (var fStream = await mergedImage.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    await cvrTarget.SaveAsync(fStream, CanvasBitmapFileFormat.Png, 1f);
+                }                
+            }
+    
+
+            
+
+        
         //event handler to share image
         private void Share_Click(object sender, RoutedEventArgs e)
         {
-
+            //will implement sharing function later
         }
+
+        
     }
 }
